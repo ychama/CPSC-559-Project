bd16b555453bf170937a586d1dc653e5ec26ec51
Rohit <rohityeast@gmail.com>
2023-02-21 21:48:47 -0700

    Trying to fill in home page, compiles but nothing shows up on ~/home

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Stack, AppShell, Text } from "@mantine/core";
+import { 
+  Flex, 
+  AppShell, 
+  Text,
+  Card,
+  Image,
+  Badge,
+  Button,
+  Group,
+  useMantineTheme,
+
+} from "@mantine/core";
 import Sidebar from "../components/Sidebar";
 
 const HomePage = () => {
-  return <AppShell navbar={<Sidebar activePage='HOME' />}></AppShell>;
+  
+  const theme = useMantineTheme();
+
+  const testJson = '{"canvasName":"Thomas\' Canvas", participants:4, capacity:5, thumbNail:"https://uploads.theartofeducation.edu/flex/lesson_plans/recN3XJX9okThbftu/927253c0" }';
+  
+  var cardInfo = testJson.map(function(canvas) {
+    return {
+      canvasName: canvas.canvasName,
+      participants: canvas.participants,
+      capacity: canvas.capacity,
+      thumbNail: canvas.thumbNail,
+    };
+  });
+  
+  return <AppShell navbar={<Sidebar activePage='HOME' />}>
+    <Flex 
+      justify="flex-start"
+      align="flex-start"
+      gap="md"
+      direction="row"
+      wrap="wrap"
+    >
+      {cardInfo.map(canvas => (
+        <Card shadow="sm" p="lg" radius="md" withBorder>
+          <Card.Section>
+            <Image 
+              src={canvas.thumbNail}
+              height={160}
+              alt="Image of {canvas.canvasName}"/>
+          </Card.Section>
+
+          <Text weight={500}>canvas.canvasName</Text>
+          <Group position="apart" mt="md" mb="xs">
+            <Text size="sm" color="dimmed">
+              {canvas.participants}/{canvas.capacity} artists joined
+            </Text>
+            {canvas.participants >= canvas.capacity &&
+              <Badge color='br-turq' variant="light">
+                Canvas Full
+              </Badge>
+            }
+          </Group>
+          <Button disabled={canvas.participants >= canvas.capacity}>
+            Join
+          </Button>
+        </Card>
+      ))}
+    </Flex>
+  </AppShell>;
 };
 
 export default HomePage;
