86ea78c243772640b92121cb6c41af76006e1d9a
Thomas Kahessay <Kahessaythomas@gmail.com>
2023-02-24 19:27:42 -0700

    Added flower template on CanvasPage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { 
-  Flex, 
-  AppShell, 
+import {
+  Flex,
+  AppShell,
   Text,
   Card,
   Image,
@@ -10,60 +10,72 @@ import {
   Button,
   Group,
   useMantineTheme,
-
 } from "@mantine/core";
 import Sidebar from "../components/Sidebar";
 
 const HomePage = () => {
-  
   const theme = useMantineTheme();
 
-  const testJson = '{"canvasName":"Thomas\' Canvas", participants:4, capacity:5, thumbNail:"https://uploads.theartofeducation.edu/flex/lesson_plans/recN3XJX9okThbftu/927253c0" }';
-  
-  var cardInfo = testJson.map(function(canvas) {
-    return {
-      canvasName: canvas.canvasName,
-      participants: canvas.participants,
-      capacity: canvas.capacity,
-      thumbNail: canvas.thumbNail,
-    };
-  });
-  
-  return <AppShell navbar={<Sidebar activePage='HOME' />}>
-    <Flex 
-      justify="flex-start"
-      align="flex-start"
-      gap="md"
-      direction="row"
-      wrap="wrap"
-    >
-      {cardInfo.map(canvas => (
-        <Card shadow="sm" p="lg" radius="md" withBorder>
-          <Card.Section>
-            <Image 
-              src={canvas.thumbNail}
-              height={160}
-              alt="Image of {canvas.canvasName}"/>
-          </Card.Section>
+  const testJson =
+    '{"canvasName":"Thomas\' Canvas", participants:4, capacity:5, thumbNail:"https://uploads.theartofeducation.edu/flex/lesson_plans/recN3XJX9okThbftu/927253c0" }';
 
-          <Text weight={500}>canvas.canvasName</Text>
-          <Group position="apart" mt="md" mb="xs">
-            <Text size="sm" color="dimmed">
-              {canvas.participants}/{canvas.capacity} artists joined
-            </Text>
-            {canvas.participants >= canvas.capacity &&
-              <Badge color='br-turq' variant="light">
-                Canvas Full
-              </Badge>
-            }
-          </Group>
-          <Button disabled={canvas.participants >= canvas.capacity}>
-            Join
-          </Button>
-        </Card>
-      ))}
-    </Flex>
-  </AppShell>;
+  // var cardInfo = testJson.map(function (canvas) {
+  //   return {
+  //     canvasName: canvas.canvasName,
+  //     participants: canvas.participants,
+  //     capacity: canvas.capacity,
+  //     thumbNail: canvas.thumbNail,
+  //   };
+  // });
+
+  let test = [
+    {
+      canvasName: "Thomas",
+      participants: 4,
+      capacity: 5,
+      thumbNail:
+        "https://uploads.theartofeducation.edu/flex/lesson_plans/recN3XJX9okThbftu/927253c0",
+    },
+  ];
+
+  return (
+    <AppShell navbar={<Sidebar activePage='HOME' />}>
+      <Flex
+        justify='flex-start'
+        align='flex-start'
+        gap='md'
+        direction='row'
+        wrap='wrap'
+      >
+        {test.map((canvas) => (
+          <Card shadow='sm' p='lg' radius='md' withBorder>
+            <Card.Section>
+              <Image
+                src={canvas.thumbNail}
+                height={160}
+                alt='Image of {canvas.canvasName}'
+              />
+            </Card.Section>
+
+            <Text weight={500}>{canvas.canvasName}</Text>
+            <Group position='apart' mt='md' mb='xs'>
+              <Text size='sm' color='dimmed'>
+                {canvas.participants}/{canvas.capacity} artists joined
+              </Text>
+              {canvas.participants >= canvas.capacity && (
+                <Badge color='br-turq' variant='light'>
+                  Canvas Full
+                </Badge>
+              )}
+            </Group>
+            <Button disabled={canvas.participants >= canvas.capacity}>
+              Join
+            </Button>
+          </Card>
+        ))}
+      </Flex>
+    </AppShell>
+  );
 };
 
 export default HomePage;
