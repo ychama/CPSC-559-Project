97c45754faa83c390fc7b1217eafdd47c4916bc9
Rohit <rohityeast@gmail.com>
2023-02-24 20:58:50 -0700

    improved home page to use better mock values

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -8,61 +8,98 @@ import {
   Image,
   Badge,
   Button,
-  Group,
+  Center,
   useMantineTheme,
-
 } from "@mantine/core";
 import Sidebar from "../components/Sidebar";
 
 const HomePage = () => {
-  
+  const navigate = useNavigate();
   const theme = useMantineTheme();
 
-  const testJson = '{"canvasName":"Thomas\' Canvas", participants:4, capacity:5, thumbNail:"https://uploads.theartofeducation.edu/flex/lesson_plans/recN3XJX9okThbftu/927253c0" }';
-  
-  var cardInfo = testJson.map(function(canvas) {
-    return {
-      canvasName: canvas.canvasName,
-      participants: canvas.participants,
-      capacity: canvas.capacity,
-      thumbNail: canvas.thumbNail,
-    };
-  });
-  
-  return <AppShell navbar={<Sidebar activePage='HOME' />}>
-    <Flex 
-      justify="flex-start"
-      align="flex-start"
-      gap="md"
-      direction="row"
-      wrap="wrap"
-    >
-      {cardInfo.map(canvas => (
-        <Card shadow="sm" p="lg" radius="md" withBorder>
-          <Card.Section>
-            <Image 
-              src={canvas.thumbNail}
-              height={160}
-              alt="Image of {canvas.canvasName}"/>
-          </Card.Section>
+  const handleJoinCanvas = (id) => {
+    navigate('/work', { state: { canvasID: id }});
+  }
 
-          <Text weight={500}>canvas.canvasName</Text>
-          <Group position="apart" mt="md" mb="xs">
-            <Text size="sm" color="dimmed">
-              {canvas.participants}/{canvas.capacity} artists joined
-            </Text>
+  let cardData = [
+    {
+      "name": "Thomas' Canvas",
+      "canvasID": 124151,
+      "participants": 4,
+      "capacity": 5,
+      "thumbNail": "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+    },
+    {
+      "name": "Yassin's Canvas",
+      "canvasID": 5362352,
+      "participants": 5,
+      "capacity": 5,
+      "thumbNail": "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+    },
+    {
+      "name": "Alejo's Canvas",
+      "canvasID": 346233,
+      "participants": 10,
+      "capacity": 2,
+      "thumbNail": "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+    },
+    {
+      "name": "Yazan's Canvas",
+      "canvasID": 53923,
+      "participants": 6,
+      "capacity": 7,
+      "thumbNail": "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+    },
+    {
+      "name": "Rohit's Canvas",
+      "canvasID": 90432,
+      "participants": 1,
+      "capacity": 100,
+      "thumbNail": "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+    }
+  ]
+
+  return <AppShell navbar={<Sidebar activePage='HOME' />}>
+    <Center ml="xl" mt="md" mr="md">
+      <Flex 
+        justify="flex-start"
+        align="flex-start"
+        gap="md"
+        direction="row"
+        wrap="wrap"
+      >
+        {cardData.map(canvas => (
+          <Card shadow="sm" p="lg" radius="md" withBorder>
+            <Card.Section>
+              <Image 
+                src={canvas.thumbNail}
+                height={160}
+                alt="Image of {canvas.name}"
+              />
+            </Card.Section>
+            <Text mt="sm" weight={500}>{canvas.name}</Text>
+            {canvas.participants < canvas.capacity &&
+              <Text mb="sm" size="sm" color="dimmed">
+                {canvas.participants}/{canvas.capacity} artists joined
+              </Text>
+            }
             {canvas.participants >= canvas.capacity &&
-              <Badge color='br-turq' variant="light">
+              <Badge mb="xs" radius="sm" color='orange' variant="filled">
                 Canvas Full
               </Badge>
             }
-          </Group>
-          <Button disabled={canvas.participants >= canvas.capacity}>
-            Join
-          </Button>
-        </Card>
-      ))}
-    </Flex>
+            <Button 
+              shadow="sm" 
+              fullWidth 
+              disabled={canvas.participants >= canvas.capacity}
+              onClick={handleJoinCanvas(canvas.canvasID)}
+            >
+                Join
+            </Button>
+          </Card>
+        ))}
+      </Flex>
+    </Center>
   </AppShell>;
 };
 
