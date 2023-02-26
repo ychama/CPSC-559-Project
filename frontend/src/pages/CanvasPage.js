4e60dee0b6b46575fd305bc4aed756831b4b6062
Thomas Kahessay <Kahessaythomas@gmail.com>
2023-02-25 19:25:26 -0700

    Cleaned canvas page

--- frontend/src/pages/CanvasPage.js
+++ frontend/src/pages/CanvasPage.js
@@ -1,36 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  Stack,
-  AppShell,
-  Text,
-  Container,
-  Image,
-  Center,
-  ColorPicker,
-} from "@mantine/core";
+import React from "react";
+import { AppShell, Center } from "@mantine/core";
 import Sidebar from "../components/Sidebar";
 import SVG from "../svgcomponents/SVG.js";
 
 const CanvasPage = () => {
-  const [fillColors, setFillColors] = useState(Array(22).fill("#FFFFFF"));
-  const [currentColor, setCurrentColor] = useState("#FFFFFF");
-
-  const onFillColor = (i) => {
-    let newFillColors = fillColors.slice(0);
-    newFillColors[i] = currentColor;
-    setFillColors(newFillColors);
-  };
-
   return (
-    <AppShell navbar={<Sidebar activePage="WORK" />}>
+    <AppShell navbar={<Sidebar activePage='WORK' />}>
       <Center>
-        <SVG fillColors={fillColors} onFill={onFillColor} />
-        <ColorPicker
-          format="hex"
-          value={currentColor}
-          onChange={setCurrentColor}
-        />
+        <SVG />
       </Center>
     </AppShell>
   );
