88b0b7bb0fb1d7768c631e697196fe4c1f727199
Thomas Kahessay <Kahessaythomas@gmail.com>
2023-03-05 21:40:05 -0700

    Added workspace endpoint on page load

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -12,11 +12,20 @@ import {
   useMantineTheme,
   SimpleGrid,
 } from "@mantine/core";
+import { getWorkspace } from "../backendhelpers/workspaceHelper";
 import Sidebar from "../components/Sidebar";
 
 const HomePage = () => {
   const navigate = useNavigate();
   const theme = useMantineTheme();
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/login");
+    } else {
+      getWorkspaces();
+    }
+  }, []);
 
   const handleJoinCanvas = (id) => {
     navigate("/work", { state: { canvasID: id } });
@@ -64,6 +73,17 @@ const HomePage = () => {
         "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80",
     },
   ];
+
+  const getWorkspaces = async () => {
+    try {
+      let workspaces = await getWorkspace({
+        token: localStorage.getItem("token"),
+      });
+      console.log(workspaces);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <AppShell navbar={<Sidebar activePage='HOME' />}>
