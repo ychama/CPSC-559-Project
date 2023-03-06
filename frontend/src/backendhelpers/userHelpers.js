b646c4fb6f42496a708963942ccbf05aa57bb50e
Thomas Kahessay <Kahessaythomas@gmail.com>
2023-03-05 21:40:19 -0700

    Added Sign in

--- frontend/src/backendhelpers/userHelpers.js
+++ frontend/src/backendhelpers/userHelpers.js
@@ -11,7 +11,17 @@ const endpointBase = process.env.BACKEND_URL || "http://localhost:5000/api";
 };*/
 
 export const signUp = async (reqBody) => {
-    return axios.post(endpointBase + "/users/signup/", reqBody).then((response) => {
-        return response.data;
+  return axios
+    .post(endpointBase + "/users/signup/", reqBody)
+    .then((response) => {
+      return response.data;
+    });
+};
+
+export const signIn = async (reqBody) => {
+  return axios
+    .post(endpointBase + "/users/login/", reqBody)
+    .then((response) => {
+      return response.data;
     });
 };
