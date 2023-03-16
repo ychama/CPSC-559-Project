1435bcdac1b0f81cefb6e5c6f8c5c0d0afadaf87
ychama <ychama15@gmail.com>
2023-03-15 20:27:55 -0600

    added git ignore

--- proxy/controllers/serverController.js
+++ proxy/controllers/serverController.js
@@ -1,30 +1,23 @@
 import asyncHandler from "express-async-handler";
 
-
 const SERVER_BASE_URL = "http://backend{}:5000/api";
-let AVAILABLE_SERVERS = [1,2]
-let OFFLINE_SERVERS = []
-
+let AVAILABLE_SERVERS = [1, 2];
+let OFFLINE_SERVERS = [];
 
 const getServer = asyncHandler(async (req, res) => {
-    try
-    {
-        const randomServer = Math.floor(Math.random() * AVAILABLE_SERVERS.length);
+  try {
+    const randomServer = Math.floor(Math.random() * AVAILABLE_SERVERS.length);
 
-        let serverURL =  SERVER_BASE_URL.replaceAll('{}', AVAILABLE_SERVERS[randomServer]);
-    
-        res.status(200).json({serverURL});
-    }
+    let serverURL = SERVER_BASE_URL.replaceAll(
+      "{}",
+      AVAILABLE_SERVERS[randomServer]
+    );
 
-    catch(error){
+    res.status(200).json({ serverURL });
+  } catch (error) {
     const errMessage = error.message;
     res.status(400).json(errMessage);
-    }
-   
-
+  }
 });
 
-
-export {
-    getServer
-}
+export { getServer };
