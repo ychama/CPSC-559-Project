e1d960155ea6755774dbb8ff5d483e14fe5cd501
Thomas Kahessay <Kahessaythomas@gmail.com>
2023-03-28 20:42:42 -0600

    Changed path update functionality

--- backend/communication/ServerToServerSocket.js
+++ backend/communication/ServerToServerSocket.js
@@ -1,132 +1,140 @@
-import http from 'http';
-import { 
-    WebSocketServer, 
-    WebSocket,
-} from 'ws';
+import http from "http";
+import { WebSocketServer, WebSocket } from "ws";
 import { updateWorkspace } from "../controllers/workspaceController.js";
 
 const SERVER_CLIENT_WEBSOCKET_URL = "ws://backend{}:600{}";
 const PORT_TEMPLATE = "600{}";
 
 const localId = process.env.SERVER_ID;
-const otherIds = process.env.OTHER_SERVERS.split(',');
+const otherIds = process.env.OTHER_SERVERS.split(",");
 
-const localPort = PORT_TEMPLATE.replace(
-    /{}/g,
-    localId
-);
+const localPort = PORT_TEMPLATE.replace(/{}/g, localId);
 
 const serverConnections = {};
 
 const listenForServers = async () => {
+  const server = http.createServer();
 
-    const server = http.createServer();
+  // overwrite this port when we create the web socket server
+  server.listen(localId, "0.0.0.0", function () {});
 
-    // overwrite this port when we create the web socket server
-    server.listen(localId, "0.0.0.0", function(){
+  // Set up server
+  const webSocketServer = new WebSocketServer({
+    port: localPort,
+    httpServer: server,
+  });
+  console.log(`Websocket Server open to other servers on port ${localPort}`);
 
+  // Connection from another server
+  webSocketServer.on("connection", function connection(superSocket) {
+    superSocket.on("message", function incoming(message) {
+      processIncomingMessage(superSocket, message);
     });
-
-    // Set up server
-    const webSocketServer = new WebSocketServer({ 
-        port: localPort, 
-        httpServer: server 
-    });
-    console.log(`Websocket Server open to other servers on port ${localPort}`);
-
-    // Connection from another server
-    webSocketServer.on('connection', function connection(superSocket) {
-
-        superSocket.on('message', function incoming(message) {
-            processIncomingMessage(superSocket, message);
-        });
-    });
+  });
 };
 
 const connectToOtherServers = async () => {
-    
-    // Wait for other servers to start
-    await delay(10000);
+  // Wait for other servers to start
+  await delay(10000);
 
-    otherIds.forEach(id => {
+  otherIds.forEach((id) => {
+    // Connect to other servers with a greater Id then this one
+    if (localId < id) {
+      const serverAddress = SERVER_CLIENT_WEBSOCKET_URL.replace(/{}/g, id);
 
-        // Connect to other servers with a greater Id then this one
-        if(localId < id) {
-            const serverAddress = SERVER_CLIENT_WEBSOCKET_URL.replace(
-                /{}/g,
-                id
-            );
-            
-            const inferiorSocket = new WebSocket(serverAddress);
-            
-            inferiorSocket.addEventListener('open', function (event) {
+      const inferiorSocket = new WebSocket(serverAddress);
 
-                console.log(`Connected to server ${id}`);
+      inferiorSocket.addEventListener("open", function (event) {
+        console.log(`Connected to server ${id}`);
 
-                // send local id
-                inferiorSocket.send(`{ "serverId": ${localId} }`);
+        // send local id
+        inferiorSocket.send(`{ "serverId": ${localId} }`);
 
-                // store connection
-                serverConnections[id] = inferiorSocket;
-            });
-            
-            // receive message
-            inferiorSocket.addEventListener('message', function (event) {
-                processIncomingMessage(inferiorSocket, event.data);
-            });
-        }
-    });
-};
+        // store connection
+        serverConnections[id] = inferiorSocket;
+      });
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
-
-async function broadcastUpdate(timeStamp, workspaceCode, paths) {
-
-    const update = {
-        timeStamp: timeStamp,
-        workspaceCode: workspaceCode,
-        paths: paths
-    };
-    const jsonUpdate = JSON.stringify(update);
-
-    for (let id in serverConnections) {
-
-        serverConnections[id].send(jsonUpdate);
+      // receive message
+      inferiorSocket.addEventListener("message", function (event) {
+        processIncomingMessage(inferiorSocket, event.data);
+      });
     }
+  });
 };
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+// async function broadcastUpdate(timeStamp, workspaceCode, paths) {
+//   const update = {
+//     timeStamp: timeStamp,
+//     workspaceCode: workspaceCode,
+//     paths: paths,
+//   };
+//   const jsonUpdate = JSON.stringify(update);
+
+//   for (let id in serverConnections) {
+//     serverConnections[id].send(jsonUpdate);
+//   }
+// }
+
+async function broadcastUpdate(timeStamp, workspaceCode, data) {
+  const update = {
+    timeStamp: timeStamp,
+    workspaceCode: workspaceCode,
+    path_id: data["path_id"],
+    color: data["color"],
+  };
+  const jsonUpdate = JSON.stringify(update);
+
+  for (let id in serverConnections) {
+    serverConnections[id].send(jsonUpdate);
+  }
+}
 
 async function processIncomingMessage(socket, message) {
+  try {
+    const jsonMsg = JSON.parse(message);
 
-    try{
-        const jsonMsg = JSON.parse(message);
+    // This is a server on the client side of the connection telling us what server they are
+    if (jsonMsg.hasOwnProperty("serverId")) {
+      console.log(`Connected to server ${jsonMsg.serverId}`);
 
-        // This is a server on the client side of the connection telling us what server they are
-        if(jsonMsg.hasOwnProperty("serverId")) {
+      // Save connection with id
+      serverConnections[jsonMsg["serverId"]] = socket;
 
-            console.log(`Connected to server ${jsonMsg.serverId}`);
-
-            // Save connection with id
-            serverConnections[jsonMsg['serverId']] = socket;
-
-        // Update message from other server
-        } else if (
-            jsonMsg.hasOwnProperty("timeStamp") &&
-            jsonMsg.hasOwnProperty("workspaceCode") &&
-            jsonMsg.hasOwnProperty("paths")) {
-            
-            updateWorkspace(jsonMsg['workspaceCode'], jsonMsg['paths'], false, jsonMsg['timeStamp']);
-
-        } else {
-            throw new Error("Unrecognized message received from another server");
-        }
-
-    } catch(error) {
-        console.log(`Error processing ws message from server: ${error}, ${error.stack}`);
+      // Update message from other server
+    } else if (
+      jsonMsg.hasOwnProperty("timeStamp") &&
+      jsonMsg.hasOwnProperty("workspaceCode") &&
+      jsonMsg.hasOwnProperty("paths")
+    ) {
+      updateWorkspace(
+        jsonMsg["workspaceCode"],
+        jsonMsg["paths"],
+        false,
+        jsonMsg["timeStamp"]
+      );
+    } else if (
+      jsonMsg.hasOwnProperty("timeStamp") &&
+      jsonMsg.hasOwnProperty("workspaceCode") &&
+      jsonMsg.hasOwnProperty("path_id") &&
+      jsonMsg.hasOwnProperty("color")
+    ) {
+      updateWorkspace(
+        jsonMsg["workspaceCode"],
+        jsonMsg["path_id"],
+        jsonMsg["color"],
+        false,
+        jsonMsg["timeStamp"]
+      );
+    } else {
+      throw new Error("Unrecognized message received from another server");
     }
-};
+  } catch (error) {
+    console.log(
+      `Error processing ws message from server: ${error}, ${error.stack}`
+    );
+  }
+}
 
-export { 
-    listenForServers, 
-    connectToOtherServers,
-    broadcastUpdate,
-};
+export { listenForServers, connectToOtherServers, broadcastUpdate };
--- backend/communication/ToFrontendSocket.js
+++ backend/communication/ToFrontendSocket.js
@@ -1,12 +1,12 @@
-import http from 'http';
-import { WebSocketServer } from 'ws';
+import http from "http";
+import { WebSocketServer } from "ws";
 import {
-    getWorkspace,
-    updateWorkspace,
-  } from "../controllers/workspaceController.js";
+  getWorkspace,
+  updateWorkspace,
+} from "../controllers/workspaceController.js";
 
 // Clients reach us at this port
-const socketPort = 7000; 
+const socketPort = 7000;
 
 // maps workspace id to an array of client connections
 let workspaceToConnection = {};
@@ -15,101 +15,107 @@ let workspaceToConnection = {};
 let connectionToWorkspace = {};
 
 const startFrontendSocket = async () => {
-    
-    const server = http.createServer();
+  const server = http.createServer();
 
-    const wsServer = new WebSocketServer({ server });
-    server.listen(socketPort, () => {
-        console.log(`WebSocket server is running on port ${socketPort}`);
+  const wsServer = new WebSocketServer({ server });
+  server.listen(socketPort, () => {
+    console.log(`WebSocket server is running on port ${socketPort}`);
+  });
+
+  // On Client Connected
+  wsServer.on("connection", function (connection) {
+    console.log("User joined workspace");
+
+    // Receive messages
+    connection.on("message", function incoming(message) {
+      processMessage(connection, message);
     });
 
-    // On Client Connected
-    wsServer.on('connection', function(connection) {
+    // FE disconnected
+    connection.on("close", (code, reason) => {
+      console.log(
+        `Front end disconnected with code ${code} because: "${reason}"`
+      );
 
-        console.log('User joined workspace');
+      // get rid of client
+      var workSpace = connectionToWorkspace[connection];
 
-        // Receive messages
-        connection.on('message', function incoming(message){
-            processMessage(connection, message);
-        });
-
-        // FE disconnected
-        connection.on('close', (code, reason) => {
-
-            console.log(`Front end disconnected with code ${code} because: "${reason}"`);
-
-            // get rid of client
-            var workSpace = connectionToWorkspace[connection];
-
-            // remove the connection from the workspace
-            if(Array.isArray(workspaceToConnection[workSpace])) {
-                const connectionIndex = workspaceToConnection[workSpace].indexOf(connection);
-                if(connectionIndex > -1) {
-                    workspaceToConnection[workSpace].splice(connectionIndex, 1);
-                }
-            }
-            delete connectionToWorkspace[connection];
-        });
-    });
-};
-
-async function updateClients(workspace, paths) {
-
-    // Send only to clients who care about this workspace
-    const clientSockets = workspaceToConnection[workspace];
-
-    if (Array.isArray(clientSockets)) {
-
-        for (const client of clientSockets) {
-    
-            client.send(`{ "paths": ${JSON.stringify(paths)} }`);
+      // remove the connection from the workspace
+      if (Array.isArray(workspaceToConnection[workSpace])) {
+        const connectionIndex =
+          workspaceToConnection[workSpace].indexOf(connection);
+        if (connectionIndex > -1) {
+          workspaceToConnection[workSpace].splice(connectionIndex, 1);
         }
-    }
+      }
+      delete connectionToWorkspace[connection];
+    });
+  });
 };
+
+async function updateClients(workspace, data) {
+  // Send only to clients who care about this workspace
+  const clientSockets = workspaceToConnection[workspace];
+
+  // if (Array.isArray(clientSockets)) {
+  //   for (const client of clientSockets) {
+  //     client.send(`{ "paths": ${JSON.stringify(paths)} }`);
+  //   }
+  // }
+
+  if (Array.isArray(clientSockets)) {
+    for (const client of clientSockets) {
+      client.send(`{ "update_color": ${JSON.stringify(data)} }`);
+    }
+  }
+}
 
 async function processMessage(connection, message) {
-    
-    try {
-        const jsonMsg = JSON.parse(message);
+  try {
+    const jsonMsg = JSON.parse(message);
 
-        // Received new connection
-        if(jsonMsg.hasOwnProperty('workspaceCode')){
+    // Received new connection
+    if (jsonMsg.hasOwnProperty("workspaceCode")) {
+      const workspaceCode = jsonMsg["workspaceCode"];
 
-            const workspaceCode = jsonMsg['workspaceCode'];
+      console.log("Received workspace code from client");
 
-            console.log("Received workspace code from client");
+      // map workspace to the connection
+      if (workspaceCode in workspaceToConnection) {
+        workspaceToConnection[workspaceCode].push(connection);
+      } else {
+        workspaceToConnection[workspaceCode] = [connection];
+      }
 
-            // map workspace to the connection
-            if(workspaceCode in workspaceToConnection) {
+      // map connection to the workspace
+      connectionToWorkspace[connection] = workspaceCode;
 
-                workspaceToConnection[workspaceCode].push(connection);
-            }
-            else {
-                
-                workspaceToConnection[workspaceCode] = [connection];
-            }
+      // send workspace they want
+      const workspace = await getWorkspace(workspaceCode);
+      connection.send(workspace);
 
-            // map connection to the workspace
-            connectionToWorkspace[connection] = workspaceCode;
-
-            // send workspace they want
-            const workspace = await getWorkspace(workspaceCode);
-            connection.send(workspace);
-
-        // Client updated workspace 
-        } else if (jsonMsg.hasOwnProperty('paths')) {
-
-            updateWorkspace(connectionToWorkspace[connection], jsonMsg["paths"], true);
-        } else {
-            throw new Error(`Unrecognize message receivedfrom client: ${message}`);
-        }
-
-    } catch (error) {
-        console.log(`Error processing ws message from FE: ${error}, ${error.stack}`);
+      // Client updated workspace
+    } else if (jsonMsg.hasOwnProperty("paths")) {
+      updateWorkspace(
+        connectionToWorkspace[connection],
+        jsonMsg["paths"],
+        true
+      );
+    } else if (jsonMsg.hasOwnProperty("update_color")) {
+      updateWorkspace(
+        connectionToWorkspace[connection],
+        jsonMsg["update_color"]["path_id"],
+        jsonMsg["update_color"]["color"],
+        true
+      );
+    } else {
+      throw new Error(`Unrecognize message receivedfrom client: ${message}`);
     }
-};
+  } catch (error) {
+    console.log(
+      `Error processing ws message from FE: ${error}, ${error.stack}`
+    );
+  }
+}
 
-export {
-    startFrontendSocket,
-    updateClients,
-};
+export { startFrontendSocket, updateClients };
