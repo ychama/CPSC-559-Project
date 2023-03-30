a8a3d799ffffb78ec8a0cbea10ba752252916d79
Thomas Kahessay <44825191+thomaskahessay@users.noreply.github.com>
2023-03-29 19:14:17 -0600

    Changed path update functionality (#16)

--- frontend/src/svgcomponents/SVG.js
+++ frontend/src/svgcomponents/SVG.js
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from "react";
 import { Center, ColorPicker, Stack, TextInput, Title } from "@mantine/core";
-import { getBackendUrl } from "../backendhelpers/proxyHelper.js"
+import { getBackendUrl } from "../backendhelpers/proxyHelper.js";
 
 let websocketUrl = localStorage.getItem("websocketURL");
 
@@ -14,24 +14,34 @@ const SVG = () => {
   const [currentColor, setCurrentColor] = useState("#FFFFFF");
   const [reconnectToSocket, setReconnectToSocket] = useState(false);
 
-  const updateColor = (index) => {
-    
+  const updateColor = (pathId) => {
     let newSVGPaths = SVGPaths.slice(0);
-    newSVGPaths[index].svgFill = currentColor;
+    newSVGPaths = newSVGPaths.map((path) => {
+      return path._id === pathId ? { ...path, svgFill: currentColor } : path;
+    });
     setSVGPaths(newSVGPaths);
 
-    try{
-      socket.send(`{ "paths": ${JSON.stringify(SVGPaths)} }`);
-    }catch(error){
+    let svgReq = {
+      path_id: pathId,
+      color: currentColor,
+    };
+
+    try {
+      socket.send(`{"update_color": ${JSON.stringify(svgReq)}}`);
+    } catch (error) {
       console.error(`Could not send color update: ${error}`);
     }
+
+    // try {
+    //   socket.send(`{ "paths": ${JSON.stringify(SVGPaths)} }`);
+    // } catch (error) {
+    //   console.error(`Could not send color update: ${error}`);
+    // }
   };
 
   useEffect(() => {
-
     // Get new backend URLs on server side failure
     async function getNewUrls() {
-
       const urls = await getBackendUrl();
       localStorage.setItem("backendURL", urls.serverURL);
       localStorage.setItem("websocketURL", urls.websocketURL);
@@ -39,58 +49,71 @@ const SVG = () => {
       console.log("Server urls reset, reconnecting...");
 
       setReconnectToSocket(!reconnectToSocket); // When this changes we reload the effect
-    };
+    }
 
     websocketUrl = localStorage.getItem("websocketURL");
     socket = new WebSocket(websocketUrl);
 
     // On connection
     const onConnected = () => {
-      socket.send(`{ "workspaceCode": "${localStorage.getItem("workspaceCode")}" }`);
+      socket.send(
+        `{ "workspaceCode": "${localStorage.getItem("workspaceCode")}" }`
+      );
     };
-    socket.addEventListener('open', onConnected);
-    
+    socket.addEventListener("open", onConnected);
+
     // On message received
     const onMessageReceived = (event) => {
-      try {        
+      try {
         const jsonMsg = JSON.parse(event.data);
-        
-        if(jsonMsg.hasOwnProperty("workspaceName") &&
-           jsonMsg.hasOwnProperty("paths") &&
-           jsonMsg.hasOwnProperty("groupTransform")) {
-              
-          setSVGTitleName(jsonMsg['workspaceName']);
-          setSVGPaths(jsonMsg['paths']);
-          setGroupTransform(jsonMsg['groupTransform']);
-          
+        console.log(jsonMsg);
+
+        if (
+          jsonMsg.hasOwnProperty("workspaceName") &&
+          jsonMsg.hasOwnProperty("paths") &&
+          jsonMsg.hasOwnProperty("groupTransform")
+        ) {
+          setSVGTitleName(jsonMsg["workspaceName"]);
+          setSVGPaths(jsonMsg["paths"]);
+          // Store in localstorage cause the useState will not be set for updates
+          localStorage.setItem("paths", JSON.stringify(jsonMsg["paths"]));
+          setGroupTransform(jsonMsg["groupTransform"]);
         } else if (jsonMsg.hasOwnProperty("paths")) {
-          
-          setSVGPaths(jsonMsg['paths']);
-          
+          setSVGPaths(jsonMsg["paths"]);
+        } else if (jsonMsg.hasOwnProperty("update_color")) {
+          // Had to retrieve from local storage cause the useState is not set yet
+          let tempSVGPaths = [...JSON.parse(localStorage.getItem("paths"))];
+          tempSVGPaths = tempSVGPaths.map((path) => {
+            if (path._id === jsonMsg["update_color"]["path_id"]) {
+              return { ...path, svgFill: jsonMsg["update_color"]["color"] };
+            }
+            return path;
+          });
+          console.log(tempSVGPaths);
+          localStorage.setItem("paths", JSON.stringify(tempSVGPaths));
+          setSVGPaths(tempSVGPaths);
         } else {
           throw new Error(`Could not parse websocket update`);
         }
-      } catch(error) {
+      } catch (error) {
         console.error(`Error receiving message: ${error}`);
       }
     };
-    socket.addEventListener('message', onMessageReceived);
-    
+    socket.addEventListener("message", onMessageReceived);
+
     // On disconnect
     const onDisconnected = () => {
-      console.log('Disconnected from the WebSocket server, reconnecting...');
+      console.log("Disconnected from the WebSocket server, reconnecting...");
       getNewUrls();
     };
-    socket.addEventListener('close', onDisconnected);
+    socket.addEventListener("close", onDisconnected);
 
     return () => {
-
-      if(socket != null) {
-
+      if (socket != null) {
         // unsub from events
-        socket.removeEventListener('open', onConnected);
-        socket.removeEventListener('message', onMessageReceived);
-        socket.removeEventListener('close', onDisconnected);
+        socket.removeEventListener("open", onConnected);
+        socket.removeEventListener("message", onMessageReceived);
+        socket.removeEventListener("close", onDisconnected);
 
         // disconnection
         socket.close(1000, `User left workspace`);
@@ -130,7 +153,9 @@ const SVG = () => {
                     d={path.svgD}
                     stroke='#000000'
                     transform={path.svgTransform}
-                    onClick={() => updateColor(index)}
+                    onClick={() => {
+                      updateColor(path._id);
+                    }}
                     fill={path.svgFill ? path.svgFill : "#FFFFFF"}
                   />
                 );
